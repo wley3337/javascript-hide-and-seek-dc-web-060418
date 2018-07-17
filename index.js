c4474357ef8097506c0fc39d6b93d74a1d2e2bb7
wley3337 <wley3337@gmail.com>
2018-07-17 18:26:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+
+function nestedTarget(){
+  return document.querySelector(`#nested .target`);
+}
+
+function increaseRankBy(n){
+  const allRankedListItems = document.querySelectorAll('.ranked-list li');
+  for(let i = 0; i < allRankedListItems.length; i++){
+    allRankedListItems[i].innerHTML = (n + parseInt(allRankedListItems[i].innerHTML));
+  }
+}
+
+
+ function deepestChild(){
+  const lastElement = document.getElementById("grand-node").querySelectorAll("div");
+  return lastElement[lastElement.length -1];
+ }
